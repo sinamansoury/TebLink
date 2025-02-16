8acd978f68d6f0f7397b899c8a704d7941ff323a
sina <sinamansoury@gmail.com>
2025-02-16 16:52:53 +0330

    add static root

--- reserving/settings.py
+++ reserving/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from django.contrib import staticfiles
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,12 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=(
-    BASE_DIR / 'static',
-)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ staticfiles/js/main.js
@@ -0,0 +1,33 @@
+const API_BASE_URL = "https://teblink.onrender.com/api/consultant/";
+
+async function fetchSpecialities() {
+    try {
+        const response = await fetch(API_BASE_URL);
+        const data = await response.json(); // دریافت داده‌ها از API
+        $('#specialities').empty(); // پاک کردن اطلاعات قبلی
+
+        data.forEach(consultant => {
+            // لینک به صفحه دکترهای مربوط به تخصص
+            const specialityLink = `/doctor/${consultant.consultant}`;
+            $('#specialities').append(`
+                <a href="${specialityLink}" class="block speciality-link" data-link="${specialityLink}">
+                    <div class="bg-white p-5 rounded-xl shadow-md border border-gray-200 hover:shadow-lg transition duration-300 transform hover:-translate-y-1">
+                        <p class="text-blue-600 text-lg font-semibold text-center">🔬 ${consultant.consultant}</p>
+                        <p class="text-gray-500 text-sm text-center mt-1">👨‍⚕️ تعداد پزشکان: <span class="font-bold">${consultant.count}</span></p>
+                    </div>
+                </a>
+            `);
+        });
+        // اضافه کردن رویداد کلیک به هر لینک
+        $('.speciality-link').on('click', function () {
+            const specialityName = $(this).data('link').split('/').pop();  // استخراج نام تخصص از لینک
+            fetchDoctorsBySpeciality(specialityName); // فراخوانی تابع برای دریافت دکترها
+        });
+
+    } catch (error) {
+        console.error("Error fetching specialities:", error);
+    }
+}
+
+$(document).ready(fetchSpecialities);
+
